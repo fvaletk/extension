a465211cdff1dbf7e7253baf58398e33789daf61
FilibKoombea <filiberto.valetk@koombea.com>
2015-04-29 11:59:01 -0500

    creating a checkin

--- app/scripts/services/checkin_service.js
+++ app/scripts/services/checkin_service.js
@@ -6,12 +6,25 @@ angular
     var authenticationService = ServiceManager.resolveService("Authentication");
 
     var actions = {
-      'getSuggestedCheckins': getSuggestedCheckins
+      'getSuggestedCheckins': getSuggestedCheckins,
+      'createCheckin': createCheckin
     };
 
     function getSuggestedCheckins(successCallback, errorCallback){
       var request = $resource(BASE_URL+'/api/v2/checkin_suggestions',{},{});
       request.get({auth_token: authenticationService.getToken()}).$promise.then(
+          function (result){
+              successCallback(result);
+          },
+          function (error){
+              errorCallback(error);
+          }
+      );
+    };
+
+    function createCheckin(params, successCallback, errorCallback){
+      var request = $resource(BASE_URL+'/api/checkins',{},{});
+      request.save({auth_token: authenticationService.getToken(), checkin: params}).$promise.then(
           function (result){
               successCallback(result);
           },
